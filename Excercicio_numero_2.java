74af38a9acc35bfed68d9b29ae3bda0b55efcd6c
gustavo matos pinheiro <137089927+Custelinha102@users.noreply.github.com>
2025-05-19 17:42:43 -0300

    Add files via upload

---
+++ Excercicio_numero_2.java
@@ -0,0 +1,12 @@
+package Exercícios;
+
+public class Excercicio_numero_2 {
+	public static void main(String[] args) {
+		
+	int i = 2;
+	while (i <= 500) {
+		System.out.println(i);
+		i = i+2;
+		}
+	}
+}
---
+++ Excercicio_numero_3.java
@@ -0,0 +1,17 @@
+package Exercícios;
+
+public class Excercicio_numero_3 {
+
+	public static void main(String[] args) {
+		int i = 1, soma = 0;
+		
+		while (i<=100) {
+			soma += i;                 
+			System.out.println(soma);
+			
+			i++;
+			
+		}
+	}
+
+}
